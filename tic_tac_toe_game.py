89616a42a104d9c6b97df62c8abb15e339778a75
Eric Wang <ericwang3.1415@gmail.com>
2023-04-23 14:05:07 -0700

    Moved tictactoe into class, added function for individual move

--- tic_tac_toe_game.py
+++ tic_tac_toe_game.py
@@ -1,71 +1,70 @@
-def print_board(board):
-    """Print the current game board."""
-    for row in board:
-        print(" | ".join(row))
-        print("-" * 9)
+class TicTacToe:
+    def __init__(self):
+        self.board = [[" " for _ in range(3)] for _ in range(3)]
+        self.current_player = 'X'
 
+    def print_board(self):
+        """Print the current game board."""
+        for row in self.board:
+            print(" | ".join(row))
+            print("-" * 9)
 
-def check_winner(board):
-    """Check if there's a winner."""
-    # Check rows
-    for row in board:
-        if row[0] == row[1] == row[2] and row[0] != " ":
-            return row[0]
+    def check_winner(self):
+        """Check if there's a winner."""
+        # Check rows
+        for row in self.board:
+            if row[0] == row[1] == row[2] and row[0] != " ":
+                return row[0]
 
-    # Check columns
-    for col in range(3):
-        if board[0][col] == board[1][col] == board[2][col] and board[0][col] != " ":
-            return board[0][col]
+        # Check columns
+        for col in range(3):
+            if self.board[0][col] == self.board[1][col] == self.board[2][col] and self.board[0][col] != " ":
+                return self.board[0][col]
 
-    # Check diagonals
-    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != " ":
-        return board[0][0]
-    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != " ":
-        return board[0][2]
+        # Check diagonals
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] and self.board[0][0] != " ":
+            return self.board[0][0]
+        if self.board[0][2] == self.board[1][1] == self.board[2][0] and self.board[0][2] != " ":
+            return self.board[0][2]
 
-    return None
+        return None
 
-
-def is_draw(board):
-    """Check if the game is a draw."""
-    for row in board:
-        if " " in row:
-            return False
-    return True
-
-
-def main():
-    """Main function to run the game."""
-    # Initialize the game board
-    board = [[" " for _ in range(3)] for _ in range(3)]
-    current_player = "X"
-
-    print("Welcome to Tic-Tac-Toe!")
-    print_board(board)
-
-    while True:
-        print(f"Player {current_player}, enter your move (row [1-3], column [1-3]):")
-        row = int(input("Enter row: ")) - 1
-        col = int(input("Enter column: ")) - 1
-
-        if row < 0 or row > 2 or col < 0 or col > 2 or board[row][col] != " ":
+    def is_draw(self):
+        """Check if the game is a draw."""
+        for row in self.board:
+            if " " in row:
+                return False
+        return True
+    
+    def play_move(self, row, col):
+        if row < 0 or row > 2 or col < 0 or col > 2 or self.board[row][col] != " ":
             print("Invalid move. Try again.")
-            continue
+        else:
+            self.board[row][col] = self.current_player
+            self.current_player = "X" if self.current_player == "O" else "O"
 
-        board[row][col] = current_player
-        print_board(board)
+    def main(self):
+        """Main function to run the game."""
 
-        winner = check_winner(board)
-        if winner:
-            print(f"Player {winner} wins!")
-            break
+        print("Welcome to Tic-Tac-Toe!")
+        self.print_board()
 
-        if is_draw(board):
-            print("It's a draw!")
-            break
+        while True:
+            print(f"Player {self.current_player}, enter your move (row [1-3], column [1-3]):")
+            row = int(input("Enter row: ")) - 1
+            col = int(input("Enter column: ")) - 1
 
-        current_player = "X" if current_player == "O" else "O"
+            self.play_move(row, col)
+            self.print_board()
 
+            winner = self.check_winner()
+            if winner:
+                print(f"Player {winner} wins!")
+                break
+            elif self.is_draw():
+                print("It's a draw!")
+                break
 
 if __name__ == "__main__":
-    main()
+    game = TicTacToe()
+    game.main()
